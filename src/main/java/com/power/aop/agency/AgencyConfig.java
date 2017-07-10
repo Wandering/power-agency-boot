58dbc02a824dd5f2afadc9095b0b591ee0e92a8e
wuhun0301 <wuhun0301@qq.com>
2017-07-10 10:42:22 +0800

    init

---
+++ src/main/java/com/power/aop/agency/AgencyConfig.java
@@ -0,0 +1,136 @@
+package com.power.aop.agency;
+
+import com.google.common.collect.Lists;
+import com.power.entity.AgenciesEntity;
+import org.apache.commons.lang.ArrayUtils;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+import javax.annotation.PostConstruct;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Administrator on 2017/7/8.
+ */
+
+public class AgencyConfig {
+
+
+
+    private  static List<MapConfig> mapAopConfigs = Lists.newArrayList();
+    private static List<EntityConfig> entityConfigs = Lists.newArrayList();
+
+    static {
+        //init
+        mapAopConfigs.add(new MapConfig("com.power.service.*.*.queryList(..) ","agencyId",0));
+        entityConfigs.add(new EntityConfig("com.power.service..*.add(..) ","agencyId",0,AgenciesEntity.class));
+    }
+
+    /**
+     * 写入agencyId
+     * @param pointcut
+     * @param value
+     * @param args
+     */
+    public static void write(String pointcut,String value,Object[] args){
+        for (MapConfig mapConfig :mapAopConfigs){
+            if (Pattern.matches(mapConfig.getRegex(),pointcut)){
+                Object object = args[mapConfig.getIndex()];
+                if (ArrayUtils.toString(object.getClass().getInterfaces()).startsWith("java.util.Map")){
+                    Map map = (Map)object;
+                    map.put(mapConfig.getField(),value);
+                    return;
+                }
+            }
+        }
+        for (EntityConfig entityConfig : entityConfigs){
+            if (Pattern.matches(entityConfig.getRegex(),pointcut)){
+                Object object = args[entityConfig.getIndex()];
+                if (object.getClass() == entityConfig.getClazz()){
+                    Field field = ReflectionUtils.findField(entityConfig.getClazz(),entityConfig.getField());
+                    if (field != null){
+                        try {
+                            field.set(object,value);
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static class Config{
+        /**正则**/
+        private String regex;
+        /**属性**/
+        private String field;
+
+        public Config(String regex, String field) {
+            this.regex = regex;
+            this.field = field;
+        }
+
+        public String getRegex() {
+            return regex;
+        }
+
+        public void setRegex(String regex) {
+            this.regex = regex;
+        }
+
+        public String getField() {
+            return field;
+        }
+
+        public void setField(String field) {
+            this.field = field;
+        }
+
+    }
+
+    public static class MapConfig extends Config{
+
+        /**第几个参数**/
+        private int index;
+
+        public MapConfig(String regex, String field, int index) {
+            super(regex, field);
+            this.index = index;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+    }
+
+    public static class EntityConfig extends MapConfig{
+        /**类名**/
+        private Class clazz;
+
+        public EntityConfig(String regex, String field, int index, Class clazz) {
+            super(regex, field, index);
+            this.clazz = clazz;
+        }
+
+        public Class getClazz() {
+            return clazz;
+        }
+
+        public void setClazz(Class clazz) {
+            this.clazz = clazz;
+        }
+
+    }
+
+}
---
+++ src/main/java/com/power/aop/agency/AutoConfigAgency.java
@@ -0,0 +1,58 @@
+package com.power.aop.agency;
+
+import com.alibaba.fastjson.JSON;
+import io.renren.entity.SysUserEntity;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+/**
+ * Created by Administrator on 2017/6/23.
+ * 自动注入项目agencyId
+ */
+@Aspect
+@Component
+public class AutoConfigAgency {
+
+    private final static Logger logger = LoggerFactory.getLogger(AutoConfigAgency.class);
+
+    @Pointcut("execution(* com.power.service..*.*(..))")
+    public void autoConfig(){}
+
+
+    @Before("autoConfig()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        Subject subject = SecurityUtils.getSubject();
+        if (!subject.isAuthenticated()){
+            logger.debug("用户未登录，跳出切入点！");
+            return;
+        }
+        String pointcut = joinPoint.getSignature().getDeclaringTypeName();
+        logger.debug("当前切入方法为:{}",pointcut);
+        Object[] args = joinPoint.getArgs();
+
+        SysUserEntity userEntity = (SysUserEntity)subject.getPrincipal();
+        Integer agencyId = userEntity.getAgencyId();
+
+        //0为超级管理员
+        if (agencyId ==0){
+            AgencyConfig.write(pointcut,agencyId.toString(),args);
+        }
+    }
+
+}
+
+
+
+
