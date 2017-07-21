8392d23f5facc6a064a8b69934791255c4470555
wuhun0301 <wuhun0301@qq.com>
2017-07-21 10:34:47 +0800

    zookeeper加入

--- src/main/java/io/renren/RenrenApplication.java
+++ src/main/java/io/renren/RenrenApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource(locations={"classpath*:config/context.xml"})
 @SpringBootApplication
 @MapperScan(basePackages = {"io.renren.dao","com.power.dao.**.**"})
 @ComponentScan(basePackages = {"com.power","io.renren"})
