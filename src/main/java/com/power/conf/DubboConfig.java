d55f49d2dcdb96f744d723892979158892c1a4ca
wuhun0301 <wuhun0301@qq.com>
2017-07-24 09:24:54 +0800

    zookeeper加入111

---
+++ src/main/java/com/power/conf/DubboConfig.java
@@ -0,0 +1,56 @@
+package com.power.conf;
+
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import com.alibaba.dubbo.config.spring.AnnotationBean;
+import com.power.cloud.spring.ZookeeperConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.web.context.support.StandardServletEnvironment;
+
+/**
+ * Created by Administrator on 2017/7/22.
+ */
+@AutoConfigureAfter(ZookeeperConfigurer.class)
+@EnableAutoConfiguration
+@Configuration
+public class DubboConfig {
+    public static final String APPLICATION_NAME = "-agency-boot-consumer";
+    @Value("${dubbo.zk.url:mc.zk.power.com}")
+    public String url;
+
+    public static final String ANNOTATION_PACKAGE = "com.power.yuneng.nonparty.api.IPowerConsumer";
+
+    @Bean
+    public ApplicationConfig applicationConfig() {
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        applicationConfig.setName(APPLICATION_NAME);
+        return applicationConfig;
+    }
+
+    @Bean
+    public RegistryConfig registryConfig() {
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setAddress(url);
+        return registryConfig;
+    }
+
+    @Bean
+    public AnnotationBean annotationBean() {
+        AnnotationBean annotationBean = new AnnotationBean();
+        annotationBean.setPackage(ANNOTATION_PACKAGE);
+        return annotationBean;
+    }
+
+//    @Override
+//    public void setEnvironment(Environment environment) {
+//        ((StandardServletEnvironment)environment).initPropertySources();
+//    }
+}
--- src/main/java/io/renren/RenrenApplication.java
+++ src/main/java/io/renren/RenrenApplication.java
@@ -3,6 +3,8 @@ package io.renren;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
@@ -10,9 +12,13 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @MapperScan(basePackages = {"io.renren.dao","com.power.dao.**.**"})
 @ComponentScan(basePackages = {"com.power","io.renren"})
-public class RenrenApplication {
+public class RenrenApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RenrenApplication.class);
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(RenrenApplication.class, args);
 	}
 }
