bef44be4cfac76e40856bfd9246aa13538228735
天王盖地虎 <Administrator@>
2017-08-28 17:46:51 +0800

    123

---
+++ src/main/java/com/power/entity/ex/PowerStationBaseDTO.java
@@ -0,0 +1,29 @@
+package com.power.entity.ex;
+
+public class PowerStationBaseDTO {
+     
+	private Long id;
+	private String code;
+	private int channel;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public int getChannel() {
+		return channel;
+	}
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+	
+	
+	
+}
